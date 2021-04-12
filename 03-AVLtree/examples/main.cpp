5a495211d468e1d976a6ff2174c5fbdcce4f008c
Sang Yeop Han <piegeek@Sangs-MacBook-Pro.local>
2021-04-12 19:45:11 +0900

    finished insert 2

--- 03-AVLtree/examples/main.cpp
+++ 03-AVLtree/examples/main.cpp
@@ -7,20 +7,20 @@
 
 int main() {
 
-    auto bt = AVLTree<int>();
+    // auto bt = AVLTree<int>();
 
-    bt.insert(3);
-    bt.insert(1);
-    bt.insert(2);
+    // bt.insert(3);
+    // bt.insert(1);
+    // bt.insert(2);
     
-    std::cout <<"height should be 1" << std::endl;
-    std::cout <<"height is " << bt.get_height(bt.root) << std::endl;
+    // std::cout <<"height should be 1" << std::endl;
+    // std::cout <<"height is " << bt.get_height(bt.root) << std::endl;
 
-    bt.remove(4);
-    bt.remove(2);
+    // bt.remove(4);
+    // bt.remove(2);
 
-    std::cout <<"height should be 1" << std::endl;
-    std::cout <<"height is " << bt.get_height(bt.root) << std::endl;
+    // std::cout <<"height should be 1" << std::endl;
+    // std::cout <<"height is " << bt.get_height(bt.root) << std::endl;
 
 
     return 0;
