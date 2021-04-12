daa9753a38385df8b6b9859c2b0fda1c5cff8a6f
Sang Yeop Han <piegeek@Sangs-MacBook-Pro.local>
2021-04-12 21:45:22 +0900

    edited main file

--- 03-AVLtree/examples/main.cpp
+++ 03-AVLtree/examples/main.cpp
@@ -5,13 +5,26 @@
 
 #include "AVLTree.hpp"
 
+template <typename T>
+void print_avl_tree(TreeNode<T>* t, int depth = 0) {
+    int to_print = t == nullptr ? 0 : t->element;
+
+    std::cout << std::string(4 * depth, ' ') << to_print << std::endl;
+    print_avl_tree(t->left.get(), depth+1);
+    print_avl_tree(t->left.get(), depth+1);
+}
+
 int main() {
 
-    // auto bt = AVLTree<int>();
+    auto bt = AVLTree<int>();
 
-    // bt.insert(3);
-    // bt.insert(1);
-    // bt.insert(2);
+    bt.insert(3);
+    bt.insert(1);
+    bt.insert(2);
+
+    TreeNode<int>* root_ptr = bt.root.get(); 
+
+    print_avl_tree(root_ptr);
     
     // std::cout <<"height should be 1" << std::endl;
     // std::cout <<"height is " << bt.get_height(bt.root) << std::endl;
