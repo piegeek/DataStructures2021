c4c8fbe8146f60df0763f0e86212dfe8b7d7bba2
Sang Yeop Han <piegeek@Sangs-MacBook-Pro.local>
2021-04-14 23:56:09 +0900

    isAVL

--- 03-AVLtree/examples/main.cpp
+++ 03-AVLtree/examples/main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <random>
 #include <algorithm>
 
 #include "AVLTree.hpp"
+
+template <typename T>
+bool is_AVL(AVLTree<T> &bt) {
+    if (bt.root == nullptr) return false;
+
+    std::stack<TreeNode<T>*> s1;
+
+    s1.push(bt.root.get());
+
+    while (!s1.empty()) {
+
+        auto node = s1.top();
+
+        auto n_h = node->height;
+        int l_h = (node->left == nullptr) ? -1 : node->left->height;
+        int r_h = (node->right == nullptr) ? -1 : node->right->height;
+        int bf = l_h - r_h;
+        
+        if (std::abs(bf) >= 2) return false;
+
+        if (n_h != std::max(l_h, r_h) + 1) return false;
+
+        s1.pop();
+
+        auto ele = node->element;
+
+        if (node->left != nullptr) {
+//            REQUIRE(ele > node->left->element);
+            if (!(ele > node->left->element))  return false;
+            s1.push(node->left.get());
+        }
+        if (node->right != nullptr) {
+//            REQUIRE(ele < node->right->element);
+            if (!(ele < node->right->element)) return false;
+            
+            s1.push(node->right.get());
+        }
+    }
+
+    return true;
+}
 
 template <typename T>
 void print_avl_tree(TreeNode<T>* t, int depth = 0) {
@@ -27,9 +69,8 @@ int main() {
     TreeNode<int>* root_ptr = bt.root.get(); 
 
     for (auto i = 1; i <= 10; i++) {
+        std::cout << is_AVL(bt) << std::endl;
         bt.remove(i);
-
-        print_avl_tree(root_ptr);
     }
 
     // TreeNode<int>* root_ptr = bt.root.get(); 
