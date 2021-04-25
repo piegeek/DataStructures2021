9a4d39fa3b61c2834f0efdf8c44a70b99d0cfa9f
Sang Yeop Han <piegeek@Sangs-MacBook-Pro.local>
2021-04-25 16:45:48 +0900

    main.cpp change

--- 04-btree/examples/main.cpp
+++ 04-btree/examples/main.cpp
@@ -8,66 +8,21 @@ int main(int argc, char *argv[]) {
     BTree<int, 3> btree;
     std::vector<int> xs;
 
-    // for (auto i = 1; i <= 10'000; i++)
-    //     xs.emplace_back(i);
+    for (auto i = 1; i <= 100; i++)
+        xs.emplace_back(i);
 
-    // std::random_device rd;
-    // std::mt19937 g(rd());
+    std::random_device rd;
+    std::mt19937 g(rd());
 
-    // std::shuffle(xs.begin(), xs.end(), g);
+    std::shuffle(xs.begin(), xs.end(), g);
 
-    // for (auto i : xs)
-    //     btree.insert(i);
+    for (auto i : xs)
+        btree.insert(i);
 
-    // btree.insert(2);
-    // btree.insert(1);
-    // btree.insert(4);
-    // btree.insert(5);
-    // btree.insert(3);
-    // btree.insert(6);
-    // btree.insert(7);
-    // btree.insert(8);
-    // btree.insert(9); // **k
-    // btree.insert(10);
-    // btree.insert(11);
-    // btree.insert(12);
+    std::shuffle(xs.begin(), xs.end(), g);
 
-    // std::cout << btree.format() << '\n';
-
-    // btree.remove(3);
-
-    // std::cout << btree.format() << '\n';
+    for (auto i : xs)
+        btree.remove(i);
     
-    // btree.remove(7);
-
-    // std::cout << btree.format() << '\n';
-    
-    // btree.remove(8);
-
-    // std::cout << btree.format() << '\n';
-    
-    // btree.remove(2);
-
-    btree.insert(20);
-    btree.insert(10);
-    btree.insert(35);
-    btree.insert(5);
-    btree.insert(15);
-    btree.insert(30);
-    btree.insert(70);
-
-    std::cout << btree.format() << '\n';
-
-    btree.remove(10);
-
-    std::cout << btree.format() << '\n';
-    
-    btree.remove(15);
-
-    std::cout << btree.format() << '\n';
-    
-    btree.remove(20);
-
-    std::cout << btree.format() << '\n';
     return 0;
 }
