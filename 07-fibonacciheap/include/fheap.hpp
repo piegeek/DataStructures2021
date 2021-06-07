7d1e40533364e1af4e6ae7886866f6c17fd24c3f
Sang Yeop Han <piegeek@Sangs-MacBook-Pro.local>
2021-06-07 21:41:59 +0900

    fibheap 9

--- 07-fibonacciheap/include/fheap.hpp
+++ 07-fibonacciheap/include/fheap.hpp
@@ -257,7 +257,7 @@ template <typename T>
 void FibonacciHeap<T>::consolidate() {
     // TODO
 
-    // const double phi = 1.61803398875;
+    const double phi = 1.61803398875;
     // size_t arr_size = std::floor(std::log(size()) / std::log(phi));
     // std::vector<std::shared_ptr<FibonacciNode<T>>> degree_arr(arr_size);
 
